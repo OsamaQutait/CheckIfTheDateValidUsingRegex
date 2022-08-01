d4cc03f8c86bc82518ce3015e4c27f86ab44050c
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-08-01 14:49:43 +0300

    update regex

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 def isValidDate(date):
-    result = re.findall("(\d{4})\.(0[1-9]|1[0-2])\.([1-2]\d|0[1-9]|3[0-1])", date)
+    result = re.findall("(\d{4})(\.|\-)(0[1-9]|1[0-2])(\.|\-)([1-2]\d|0[1-9]|3[0-1])", date)
     if result:
         if int(result[0][0]) % 100 == "00":
             if int(result[0][0]) % 4 == 0:
@@ -17,5 +17,5 @@ def isValidDate(date):
         print("the date is invalid")
 
 if __name__ == '__main__':
-    isValidDate("2000.07.31")
+    isValidDate("2000-07-31")
 
