1fa7d28c91691e4f897ddb895b77f662626e5538
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-08-02 07:47:15 +0300

    update code and add DateFile

---
+++ DateFile.py
@@ -0,0 +1,12 @@
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def getYear(self):
+        return self.year
+    def getMonth(self):
+        return self.month
+    def getDay(self):
+        return self.day
--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 import re
+from DateFile import *
 def isValidDate(date):
+    month_and_day = {1: 31,
+                     2: 28,
+                     3: 31,
+                     4: 30,
+                     5: 31,
+                     6: 30,
+                     7: 31,
+                     8: 31,
+                     9: 30,
+                     10: 31,
+                     11: 30,
+                     12: 31}
+    leapYearFalg = False
+    validDateFlag = False
     result = re.findall("(\d{4})(\.|\-)(0[1-9]|1[0-2])(\.|\-)([1-2]\d|0[1-9]|3[0-1])", date)
+    date = Date(int(result[0][0]), int(result[0][2]), int(result[0][4]))
     if result:
-        if int(result[0][0]) % 100 == "00":
-            if int(result[0][0]) % 4 == 0:
-                print("Leap year ^_^")
+        if date.getYear() % 100 == "00":
+            if date.getYear() % 4 == 0:
+                leapYearFalg = True
             else:
-                print("Not Leap year")
+                leapYearFalg = False
         else:
-            if int(result[0][0]) % 400 == 0:
-                print("Leap year ^_^")
+            if date.getYear() % 400 == 0:
+                leapYearFalg = True
+                #print("Leap year ^_^")
             else:
-                print("Not Leap year")
-        print("the date is valid")
+                leapYearFalg = False
+                #print("Not Leap year")
+        if leapYearFalg:
+            if date.getMonth() == 2:
+                if date.getDay() <= 29:
+                    validDateFlag = True
+                else:
+                    validDateFlag = False
+            elif month_and_day[date.getMonth()] >= date.getDay():
+                validDateFlag = True
+            else:
+                validDateFlag = False
+        else:
+            if month_and_day[date.getMonth()] >= date.getDay():
+                validDateFlag = True
+            else:
+                validDateFlag = False
+        #print("the date is valid")
+    else:
+        print("the date is invalid")
+    if leapYearFalg and validDateFlag:
+        print("Leap year and the date is valid ^_^")
     else:
         print("the date is invalid")
 
 if __name__ == '__main__':
-    isValidDate("2000-07-31")
+    isValidDate("2001.02.29")
 
